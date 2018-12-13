c84db96b17cc72c28e8c61063959a262b9a29b84
Chris Russo <crusso@wesleyan.edu>
2018-12-13 11:48:01 -0500

    Initial commit

---
+++ R/fib_memoized.R
@@ -0,0 +1,36 @@
+#'@title Memoized Fibonacci
+#'
+#'@description
+#'\code{fib_memoized} computes the nth fibonacci number (1-indexed)
+#'
+#'@details
+#'This function uses memoization and is more efficient than \code{fib}
+#'
+#'@param n an integer
+#'@param newlist createList(n)
+#'
+#'@export
+#'
+#'@return the nth fibonacci number
+#'
+#'@authors Shota Nakamura <snakamura@@wesleyan.edu>, Chris Russo <crusso@@wesleyan.edu>
+#'
+#'@examples
+#'fib_memoized(10,createList(10))[[1]]
+
+fib_memoized <- function(n,newlist){
+  if(n == 1){
+    return(newlist[[1]])
+  } else if(n == 2){
+    return(newlist[[2]])
+  } else{
+    if(newlist[[n]] != -1){
+      return(list(newlist[[n]],newlist))
+    } else{
+      newlist[[n-2]] = fib_memoized(n-2,newlist)[[1]]
+      newlist[[n-1]] = fib_memoized(n-1,newlist)[[1]]
+      newlist[[n]] = fib_memoized(n-2,newlist)[[1]] + fib_memoized(n-1,newlist)[[1]]
+    }
+  }
+  return(list(newlist[[n]],newlist))
+}
---
+++ R/mult.R
@@ -0,0 +1,29 @@
+#'@title Multiply By Recursive Addition
+#'
+#'@description
+#'\code{mult} multiplies the first and second parameter
+#'and returns result
+#'
+#'@details
+#'This function does the same operation as * but by recursively adding values
+#'
+#'@param x first value for multiplication
+#'@param y second value for multiplication
+#'
+#'@export
+#'
+#'@return the result of x multiplied by y
+#'
+#'@authors Shota Nakamura <snakamura@@wesleyan.edu>, Chris Russo <crusso@@wesleyan.edu>
+#'
+#'@examples
+#'
+#'mult(2,3)
+
+mult <- function(x, y){
+  if(y == 0){
+    0
+  } else{
+    x + mult(x, y - 1)
+  }
+}
